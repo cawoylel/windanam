d9bdfe73ff3385c35868b0f42e16c27481d0c9ae
yaya-sy <yayasysco@gmail.com>
2024-02-25 18:10:46 +0100

    typo

--- src/data2vec/pretrain.sh
+++ src/data2vec/pretrain.sh
@@ -1,7 +1,7 @@
 python fairseq/fairseq_cli/hydra_train.py -m \
 --config-dir configs \
 --config-name data2vec_300m.yaml \
-task.data=prepared\
+task.data=prepared \
 distributed_training.distributed_world_size=1 \
 optimization.update_freq='[16]' \
 common.user_dir=examples/data2vec
