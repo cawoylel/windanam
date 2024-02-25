4eca17dbc7937b7eafed15fe87d58eb8e643526c
yaya-sy <yayasysco@gmail.com>
2024-02-25 18:09:34 +0100

    sh scripts

--- src/data2vec/pretrain.sh
+++ src/data2vec/pretrain.sh
@@ -1,10 +1,7 @@
 python fairseq/fairseq_cli/hydra_train.py -m \
---config-dir windanam/configs \
+--config-dir configs \
 --config-name data2vec_300m.yaml \
-dataset.batch_size=1 \
-task.data=prepared \
-dataset.validate_interval_updates=200 \
-optimization.max_update=40_000 \
+task.data=prepared\
 distributed_training.distributed_world_size=1 \
 optimization.update_freq='[16]' \
 common.user_dir=examples/data2vec
